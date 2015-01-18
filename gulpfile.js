f04c9e627de84d6b00d70cbff4cfad22e7805cba
Ezekiel Kigbo <theatlasroom@gmail.com>
2015-01-18 12:17:53 +1100

    working on basics of the design

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var bowerFiles = require('main-bower-files');
+var sequence = require('run-sequence');
 var uglify = require('gulp-uglify');
 var clean = require('gulp-clean');
 var lessc = require('gulp-less');
@@ -9,31 +11,38 @@ var minfycss = require('gulp-minify-css');
 var sourcemaps = require('gulp-sourcemaps');
 var template = require('gulp-template-compile');
 
-// precompile the templates
-gulp.task('less', function(){
-  gulp.src('./styles/build/haleyk/haleyk.less')
-  	.pipe(sourcemaps.init())
-    .pipe(lessc())
-    .pipe(concat('haleyk-dist.css'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./styles'));
+// config paths
+var config = {
+  sassPath: './sass',
+  bowerDir: './bower_components'
+};
+
+// compile the bower files into a libs folder
+gulp.task('bower-compile', function(){
+	return gulp.src(bowerFiles())
+	  .pipe(gulp.dest('dist/libs')) // save the file
+	  .on('error', gutil.log); 
 });
 
+// precompile the templates
+gulp.task('less', function(){
+  gulp.src('./less/*.less')
+  	.pipe(sourcemaps.init())
+    .pipe(lessc())
+    .pipe(concat('eakigbo.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css'));
+});
 
 // Clean the templates
 gulp.task('clean-less', function() {
-  return gulp.src(['./styles/haleyk-dist.css','./styles/haleyk-dist.css.map'], {read: false})
+  return gulp.src(['./css/*'], {read: false})
     .pipe(clean());
-});
-
-
-gulp.task('glessc', ['clean-less'], function() {
-   gulp.start('less');
 });
 
 // precompile the templates
 gulp.task('tmpl', function(){
-  gulp.src('./scripts/haleyk/templates/**/*.html')
+  gulp.src('./js/templates/** /*.html')
     .pipe(template({namespace: 'haleyk_templates'}))
     .pipe(concat('templates.js'))
     .pipe(gulp.dest('./scripts/haleyk/templates/'));
@@ -41,8 +50,12 @@ gulp.task('tmpl', function(){
 
 // Clean the templates
 gulp.task('clean-templates', function() {
-  return gulp.src(['./script/templates/**/*.js'], {read: false})
+  return gulp.src(['./js/templates/**/*.js'], {read: false})
     .pipe(clean());
+});
+
+gulp.task('glessc', ['clean-less'], function() {
+   gulp.start('less');
 });
 
 gulp.task('templates', ['clean-templates'], function() {
@@ -50,8 +63,7 @@ gulp.task('templates', ['clean-templates'], function() {
 });
 
 gulp.task('watch', function () {
-   gulp.watch('./styles/**/*.less', ['glessc']);
-   gulp.watch('./scripts/**/*.html', ['templates']);
-   //gulp.watch('./js/templates/**/*.html', ['tmpl']);
+   gulp.watch('./less/**/*.less', ['glessc']);
+   gulp.watch('./js/templates/**/*.html', ['templates']);
 });
 
