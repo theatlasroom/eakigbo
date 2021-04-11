21809d0b35a2ac384d48ee34a1198f4fd9ee667e
Ezekiel <theatlasroom@gmail.com>
2021-04-11 18:00:09 +1000

    --wip-- [skip ci]

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/theatlasroom/eakigbo
 go 1.13
 
 require (
+	github.com/andybalholm/brotli v1.0.1 // indirect
+	github.com/gofiber/fiber v1.14.6 // indirect
+	github.com/gofiber/fiber/v2 v2.3.3 // indirect
 	github.com/joho/godotenv v1.3.0
+	github.com/klauspost/compress v1.11.7 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.19.0 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 	gopkg.in/russross/blackfriday.v2 v2.0.0
 )
--- server.go
+++ server.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"path/filepath"
-	"time"
 
+	"github.com/alecthomas/chroma/formatters/html"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
-	"gopkg.in/russross/blackfriday.v2"
 )
 
 type content struct {
@@ -19,50 +15,82 @@ type content struct {
 	data             map[string]string
 }
 
-func safeHTML(str string) template.HTML {
-	return template.HTML(str)
+// func safeHTML(str string) template.HTML {
+// 	return template.HTML(str)
+// }
+
+// func render(c *fiber.Ctx, data content) {
+// 	// create a custom function to ensure html content does not get expected
+// 	customFns := template.FuncMap{"safeHTML": safeHTML}
+// 	// parse the template file
+// 	fn, err := template.New("").Funcs(customFns).ParseFiles(c.layout, c.template)
+// 	if err != nil {
+// 		log.Print(err)
+// 	}
+
+// 	tmpl := template.Must(fn, err)
+
+// 	// apply the data to the template and write the results in the response writer
+// 	tmpl.ExecuteTemplate(c, "layout", data.data)
+// }
+
+func render(c *fiber.Ctx, data content) error {
+	return c.Render(data.template, fiber.Map{
+		"Title": "Hello, World!",
+	})
 }
 
-func render(w http.ResponseWriter, c content) {
-	// create a custom function to ensure html content does not get expected
-	customFns := template.FuncMap{"safeHTML": safeHTML}
-	// parse the template file
-	fn, err := template.New("").Funcs(customFns).ParseFiles(c.layout, c.template)
-	if err != nil {
-		log.Print(err)
+func cvRoute(c *fiber.Ctx) error {
+	// layout := filepath.Join("views", "layouts", "main.handlebars")
+	// fp := filepath.Join("views", "cv.handlebars")
+
+	data := &content{
+		template: "cv",
+		// data:     newRouteData(renderMarkdown("md/cv.md")),
+		data: "",
 	}
 
-	tmpl := template.Must(fn, err)
-
-	// apply the data to the template and write the results in the response writer
-	tmpl.ExecuteTemplate(w, "layout", c.data)
+	render(c, *data)
 }
 
-func cvRoute(w http.ResponseWriter, r *http.Request) {
-	layout := filepath.Join("views", "layouts", "main.handlebars")
-	fp := filepath.Join("views", "cv.handlebars")
+func indexRoute(c *fiber.Ctx) error {
+	// layout := filepath.Join("views", "layouts", "main.handlebars")
+	// fp := filepath.Join("views", "index.handlebars")
 
-	c := &content{
-		layout:   layout,
-		template: fp,
-		data:     newRouteData(renderMarkdown("md/cv.md")),
+	data := &content{
+		template: "index",
+		// data:     newRouteData(renderMarkdown("md/about.md")),
+		data: "",
 	}
 
-	render(w, *c)
+	render(c, *data)
 }
 
-func indexRoute(w http.ResponseWriter, r *http.Request) {
-	layout := filepath.Join("views", "layouts", "main.handlebars")
-	fp := filepath.Join("views", "index.handlebars")
+// func cvRoute(w http.ResponseWriter, r *http.Request) {
+// 	layout := filepath.Join("views", "layouts", "main.handlebars")
+// 	fp := filepath.Join("views", "cv.handlebars")
 
-	c := &content{
-		layout:   layout,
-		template: fp,
-		data:     newRouteData(renderMarkdown("md/about.md")),
-	}
+// 	c := &content{
+// 		layout:   layout,
+// 		template: fp,
+// 		data:     newRouteData(renderMarkdown("md/cv.md")),
+// 	}
 
-	render(w, *c)
-}
+// 	render(w, *c)
+// }
+
+// func indexRoute(w http.ResponseWriter, r *http.Request) {
+// 	layout := filepath.Join("views", "layouts", "main.handlebars")
+// 	fp := filepath.Join("views", "index.handlebars")
+
+// 	c := &content{
+// 		layout:   layout,
+// 		template: fp,
+// 		data:     newRouteData(renderMarkdown("md/about.md")),
+// 	}
+
+// 	render(w, *c)
+// }
 
 func newRouteData(content string) map[string]string {
 	return map[string]string{
@@ -77,15 +105,17 @@ func newRouteData(content string) map[string]string {
 	}
 }
 
-func renderMarkdown(file string) string {
-	// TODO: cache rendered markdown
-	input, err := ioutil.ReadFile(file)
-	if err != nil {
-		log.Print(err)
-		return ""
-	}
-	return string(blackfriday.Run(input))
-}
+func markdownToHtml() {}
+
+// func renderMarkdown(file string) string {
+// 	// TODO: cache rendered markdown
+// 	input, err := ioutil.ReadFile(file)
+// 	if err != nil {
+// 		log.Print(err)
+// 		return ""
+// 	}
+// 	return string(blackfriday.Run(input))
+// }
 
 func main() {
 	// load envvars
@@ -94,29 +124,50 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	serverPort := os.Getenv("PORT")
+	// Initialize standard Go html template engine
+	engine := html.New("./views", ".html")
 
-	// create server mux config
-	handler := http.NewServeMux()
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Use(logger.New())
 
 	// serve the contents of the public directory
-	fs := http.FileServer(http.Dir("public"))
-	handler.Handle("/static/", http.StripPrefix("/static/", fs))
+	app.Static("/static", "/public")
 
 	// Register http handlers
-	handler.HandleFunc("/cv", cvRoute)
-	handler.HandleFunc("/", indexRoute)
+	app.Get("/cv", cvRoute)
+	app.Get("*", indexRoute)
 
-	// set up server
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%s", serverPort),
-		Handler:        handler,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-
-	// serve our new shiny server
-	log.Printf("Server starting on port %s - env: %s", serverPort, os.Getenv("ENV"))
-	log.Fatal(s.ListenAndServe())
+	log.Fatal(app.Listen(":3000"))
 }
+
+// func main() {
+
+// 	serverPort := os.Getenv("PORT")
+
+// 	// create server mux config
+// 	handler := http.NewServeMux()
+
+// 	// serve the contents of the public directory
+// 	fs := http.FileServer(http.Dir("public"))
+// 	handler.Handle("/static/", http.StripPrefix("/static/", fs))
+
+// 	// Register http handlers
+// 	handler.HandleFunc("/cv", cvRoute)
+// 	handler.HandleFunc("/", indexRoute)
+
+// 	// set up server
+// 	s := &http.Server{
+// 		Addr:           fmt.Sprintf(":%s", serverPort),
+// 		Handler:        handler,
+// 		ReadTimeout:    10 * time.Second,
+// 		WriteTimeout:   10 * time.Second,
+// 		MaxHeaderBytes: 1 << 20,
+// 	}
+
+// 	// serve our new shiny server
+// 	log.Printf("Server starting on port %s - env: %s", serverPort, os.Getenv("ENV"))
+// 	log.Fatal(s.ListenAndServe())
+// }
